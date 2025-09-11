14915253fea2d8dbf159b220636d2926a46f7511
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2025-09-11 10:38:08 +0200

    Run locally Windows and also Colab with PyFMI 2.18.3

--- BPL_IEC_fmpy_explore.py
+++ BPL_IEC_fmpy_explore.py
@@ -63,7 +63,6 @@
 # 2024-11-07 - Update BPL 2.3.0
 # 2025-06-13 - Test MSL 4.1.0 with OpenModelica genreated FMU
 # 2025-07-25 - Update BPL 2.3.1
-# 2025-09-03 - Updated opts_std['ncp'] = 1000 mainly for ME (and CS) and now works with FMPy=0.3.26
 #------------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------------------------------------
@@ -121,9 +120,9 @@ elif platform.system() == 'Linux':
 
 # Provide various opts-profiles
 if flag_type in ['CS', 'cs']:
-   opts_std = {'ncp': 1000}
+   opts_std = {'ncp': 500}
 elif flag_type in ['ME', 'me']:
-   opts_std = {'ncp': 1000}
+   opts_std = {'ncp': 500}
 else:    
    print('There is no FMU for this platform')
 
